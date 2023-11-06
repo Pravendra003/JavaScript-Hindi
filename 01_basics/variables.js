b7b01dc694d0afd83ccab4fe8a85d5596f79b534
Pravendra003 <136943062+Pravendra003@users.noreply.github.com>
2023-11-06 18:09:01 +0000

    done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345678
+let accountEmail = "pkshp003@gmail.log";
+var accountPass = "121212";
+accountCity = "jaipur";
+
+
+//accountId = 987655678  //not allowed
+
+accountEmail = "kahffjd@gamil.log";
+accountPass = "999999";
+accountCity = "Benglore";
+
+console.table([accountEmail, accountPass, accountCity])
