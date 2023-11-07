f7ad42f204bbec7f6bc61242e3ea36a63555b767
Pravendra003 <136943062+Pravendra003@users.noreply.github.com>
2023-11-07 09:28:08 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all js code as newer version
+
+
+alert(3 + 3)   //we are using node.js, not browser
+
+
+/* primitive types
+
+string = "pravendra";
+number = 1234;
+bigint =
+boolean = true/false;
+null = 
+undefine = 
+symbol = 
+
+
+*/
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -10,4 +10,9 @@ accountEmail = "kahffjd@gamil.log";
 accountPass = "999999";
 accountCity = "Benglore";
 
+/*
+Prefer not to use var
+because of issues in block scope and function scope
+*/
+
 console.table([accountEmail, accountPass, accountCity])
