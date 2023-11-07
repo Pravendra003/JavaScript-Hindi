6e4ade427013aa50fa19903924aa9a2d013d9cc1
Pravendra003 <136943062+Pravendra003@users.noreply.github.com>
2023-11-07 14:23:27 +0000

    conversion

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,35 @@
+// let score = "33abc";
+
+/*
+console.log(typeof (score));
+
+let newScore = Number(score);
+console.log(typeof (newScore));
+console.log(newScore);
+*/
+
+
+/*
+"33" => 33
+"33abc" => Nan
+true => 1, false => 0
+"pravendra" => Nan
+*/
+
+/*
+let logNum = "pravendra ";
+let newLogNum = Boolean(logNum);
+console.log(newLogNum);
+*/
+
+/*
+1 => true; 0 => false
+"" => false
+" " => true
+"pravendra" => true
+*/
+
+
+//****************************  Operations  ********************************
+
+
